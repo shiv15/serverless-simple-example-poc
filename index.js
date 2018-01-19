798c01d1a16b8c55267827f6740c7d4ec6abffe5
Shivendu <shivendu@appler.xyz>
2018-01-19 13:46:05 +0530

    minor changes for pagination

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const ReqRes = require('serverless-req-res');
-const Trip = require('./models/tripMaster');
+const Customer = require('./models/customer');
 const bookshelf = require('./DB');
 
 module.exports.get = new ReqRes(async (req, res) => {
     const db = new bookshelf();
-    const trip = new Trip(db.bookshelf);
-      const model = await trip.getTrips(req.params.id);
+    const customer = new Customer(db.bookshelf);
+
+      const model = await customer.getCustomers(req.params.id, req.query.pp, req.query.pg);
       db.destroy();
       res.json(model);
   }).catch((errors, req, res) => {
-    console.log(errors)
+    console.log(errors);
     res.error(404, errors[0]);
   }).cors(true).run;
